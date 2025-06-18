596bd77942e0d970ed8e7cf253bc39ea2a9be9a7
Ric <silvafang0@gmail.com>
2025-06-18 20:43:07 +0800

    New

--- app/dashboardScreen.tsx
+++ app/dashboardScreen.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+
 
 export default function DashboardScreen() {
   return (
     <View>
       <Text>Dashboard</Text>
+      <Button title={"Logout"} />
+      <Button title={"Add Question/Answer"} />
+      <Button title={"Do Flipcard"} />
     </View>
   );
 }
